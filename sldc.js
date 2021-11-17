6963044e8285f795d2b66fb9609b7d760313d7a6
Zak Liles <zak.liles@gmail.com>
2021-11-16 22:08:49 -0700

    Software Development Problems Completed

---
+++ sldc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "After determining what the end goal of the software is, this step involves analyzing how the end goal will be achieved. Individual features and details will be discussed and clarified with the customer/client to ensure these specific requirements will fulfill the end goal. Specific requirements for a Minimum Viable Product (MVP) might be defined here."
+
+const design = "Decide how the software will look, how the user will interact with the software. Languages or frameworks used might be decided." 
+
+const implementation = "Translating design into code. Each feature and tool will be coded and built.
+
+const testingAndIntegration ="All features and tools go through rigorous testing to ensure everything works as expected. Any bugs or defects would be reported to the development team and fixed. Software should be stable, bug-free (or at least minimized) and working according to previously defined requirements by the end of this step. Software is deployed at end"
+
+const maintain = "Once software is released and being used by end-user. Any new bugs or issues are addressed. Updates might be made to improve or add features as needed."
